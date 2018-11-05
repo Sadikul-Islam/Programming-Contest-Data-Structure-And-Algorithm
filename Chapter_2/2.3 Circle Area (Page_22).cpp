69ab7374d4d5f968f0826961932e94ce3a96008b
Sadikul-Islam <netshuvo@gmail.com>
2018-11-05 11:41:43 +0600

    Added 2.3 Circle Area (Page_22).cpp

---
+++ Chapter_2/2.3 Circle Area (Page_22).cpp
@@ -0,0 +1,18 @@
+/**
+    2.3 Circle Area (Page_22).cpp
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author Md. Sadikul Islam
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r,area,pi;
+    cin>>r;
+    pi=acos(-1.);
+    area=pi*r*r;
+    cout<<area<<endl;
+
+    return 0;
+}
