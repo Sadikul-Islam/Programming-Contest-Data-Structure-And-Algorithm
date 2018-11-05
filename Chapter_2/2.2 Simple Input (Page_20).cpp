ab9ccca9ed3bdc1839747106698fb822eed7597d
Sadikul-Islam <netshuvo@gmail.com>
2018-11-05 11:41:12 +0600

    Added 2.2 Simple Input (Page_20).cpp

---
+++ Chapter_2/2.2 Simple Input (Page_20).cpp
@@ -0,0 +1,16 @@
+/**
+    2.2 Simple Input.cpp (Page_20)
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author Md. Sadikul Islam
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
+
