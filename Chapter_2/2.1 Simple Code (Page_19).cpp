c73167b62325cdfca63ad33425892a0f88e861f0
Sadikul-Islam <netshuvo@gmail.com>
2018-11-05 11:39:47 +0600

    Added 2.1 Simple Code (Page_19).cpp

---
+++ Chapter_2/2.1 Simple Code (Page_19).cpp
@@ -0,0 +1,13 @@
+/**
+    2.1 Simple Code.cpp (Page_19)
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author Md. Sadikul Islam
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<6+2<<endl;
+    return 0;
+}
