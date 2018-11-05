66d9ca2ab976e9f8d93b77ff7ed574fe2afcf439
Sadikul-Islam <netshuvo@gmail.com>
2018-11-05 12:53:04 +0600

    Added দুটি দ্বিমাত্রিক (2D) বিন্দুর স্থানাঙ্ক (Co-Ordinate) ইনপুট নিয়ে তাদের মাঝের দূরত্ব প্রিন্ট কর।

---
+++ Chapter_2/Onushiloni_01 (Page 23).cpp
@@ -0,0 +1,18 @@
+/**
+    Onushiloni_01 (Page 23)
+           দুটি দ্বিমাত্রিক (2D) বিন্দুর স্থানাঙ্ক (Co-Ordinate) ইনপুট নিয়ে তাদের মাঝের দূরত্ব প্রিন্ট কর।
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author Md. Sadikul Islam
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b,d;
+    cin>>a>>b;
+    d=a-b;
+    cout<<"Distance is: "<<d<<endl;
+
+    return 0;
+}
