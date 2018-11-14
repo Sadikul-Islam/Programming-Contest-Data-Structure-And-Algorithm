d52b48eae4ed23ebe41feeba504e415459934185
Sadikul-Islam <netshuvo@gmail.com>
2018-11-14 13:57:46 +0600

    Added একটি ত্রিভুজের তিনটি বাহুর দৈর্ঘ দেওয়া আছে, তার তিনটি পরিসীমা নির্ণয় কর।

---
+++ Chapter_2/Onushiloni_04 (Page 24).cpp
@@ -0,0 +1,19 @@
+/**
+    Onushiloni_03 (Page 24)
+            একটি ত্রিভুজের তিনটি বাহুর দৈর্ঘ দেওয়া আছে, তার তিনটি পরিসীমা নির্ণয় কর।
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r,C;
+    cout<<"Enter the radius: ";
+    cin>>r;
+    C=2*M_PI*r;
+    cout<<"Circumference is: "<<C<<endl;
+
+    return 0;
+}
