cf9e60874d1026e1869b43b13fe243c20b55c02b
Sadikul-Islam <netshuvo@gmail.com>
2018-11-15 23:39:10 +0600

    Added দুটি সংখ্যার মধ্যে বড়টি প্রিন্ট করো।

---
+++ Chapter_2/No.15__Onushiloni_Largest (Page 28).cpp
@@ -0,0 +1,25 @@
+
+/**
+    No.15__Onushiloni_Largest Number (Page 28).cpp
+           দুটি সংখ্যার মধ্যে বড়টি প্রিন্ট করো।
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double num1,num2;
+    cin>>num1>>num2;
+    if(num1>num2)
+    {
+        cout<<num1<<endl;
+    }
+    else
+    {
+        cout<<num2<<endl;
+    }
+
+    return 0;
+}
