6682bac1441ba7a2f7b48f4ffad987783ccc2cc9
Sadikul-Islam <netshuvo@gmail.com>
2018-11-16 11:54:08 +0600

    Added 2.10 Simple Loop 3 (Page 30).cpp

---
+++ Chapter_2/No.21__2.10 Simple Loop 3 (Page 30).cpp
@@ -0,0 +1,24 @@
+
+/**
+    No.21__2.10 Simple Loop 3 (Page 30).cpp
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=10,i;
+    for(i=1; i<=n; i++)
+    {
+        if(i>3)
+        {
+            break;
+        }
+        cout<<i<<endl; ///Prints only 1,2 and 3
+
+    }
+
+    return 0;
+}
