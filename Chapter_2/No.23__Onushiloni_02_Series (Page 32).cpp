52843e99968b944f6001a275effc74cedffe7139
Sadikul-Islam <netshuvo@gmail.com>
2018-11-16 12:24:23 +0600

    Added 1^2+2^2+3^2+...+n^2

---
+++ Chapter_2/No.23__Onushiloni_02_Series (Page 32).cpp
@@ -0,0 +1,18 @@
+
+/**
+    No.23__Onushiloni_02_Series (Page 32).cpp
+    1^2+2^2+3^2+...+n^2
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(n*(n+1)*(2*n+1))/6<<endl;
+
+    return 0;
+}
