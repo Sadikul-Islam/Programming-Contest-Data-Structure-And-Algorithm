848d5a40e6eab4f3e7f51eafe8c74c4430d5d1f0
Sadikul-Islam <netshuvo@gmail.com>
2018-11-16 11:41:01 +0600

    Added 2.8 Simple Loop (Page 28).cpp

---
+++ Chapter_2/No.19__2.8 Simple Loop (Page 28).cpp
@@ -0,0 +1,20 @@
+
+/**
+    No.19__2.8 Simple Loop (Page 28).cpp
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
