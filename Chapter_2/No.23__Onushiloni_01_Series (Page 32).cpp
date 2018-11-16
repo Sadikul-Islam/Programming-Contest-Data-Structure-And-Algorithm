e109c8c2952170d400beebf59f520fc730d683d4
Sadikul-Islam <netshuvo@gmail.com>
2018-11-16 12:11:14 +0600

    Added 1+2+3+...+n

---
+++ Chapter_2/No.23__Onushiloni_01_Series (Page 32).cpp
@@ -0,0 +1,18 @@
+
+/**
+    No.23__Onushiloni_01_Series (Page 32).cpp
+    1+2+3+...+n
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(n*(n+1))/2<<endl;
+
+    return 0;
+}
