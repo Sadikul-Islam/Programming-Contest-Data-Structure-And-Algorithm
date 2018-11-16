fc16c777ae6a0dc2901e721883575f91e2459912
Sadikul-Islam <netshuvo@gmail.com>
2018-11-16 16:46:22 +0600

    Added 1*n+2*(n-1)+...+n*1

---
+++ Chapter_2/No.23__Onushiloni_07_Series (Page 32).cpp
@@ -0,0 +1,18 @@
+
+/**
+    No.23__Onushiloni_07_Series (Page 32).cpp
+    1*n+2*(n-1)+...+n*1
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(n*(n+1)*(n+2))/6<<endl;
+
+    return 0;
+}
