eac1aa9e4259334fae937f2ce326b0d34c351280
Sadikul-Islam <netshuvo@gmail.com>
2018-11-16 22:20:19 +0600

    Added Onushiloni_Factorial (Page 32).cpp

---
+++ Chapter_2/No.27__Onushiloni_Factorial (Page 32).cpp
@@ -0,0 +1,21 @@
+
+/**
+    No.26__Onushiloni_Factorial (Page 32).cpp
+    n! (n ফ্যাক্টোরিয়াল)  নির্ণয় কর।
+    Purpose: Solve the book "Programming Contest, Data Structure And Algorithm" by Md. Mahbubul Hasan"
+    @author: Md. Sadikul Islam Shuvo
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,fact=1;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+       fact=fact*i;
+    }
+    cout<<fact<<endl;
+
+    return 0;
+}
